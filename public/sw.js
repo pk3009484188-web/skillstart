441d29f3a837cfd7f468e969ede93aed3e731582
Kamran <pk3009484188@gmail.com>
2026-04-22 15:14:08 +0500

    add Monetag script

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10910426
+    "zoneId": 10910554
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
